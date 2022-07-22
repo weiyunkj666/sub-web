8d4f71fcf4e25eb5d02304d6873ff3f09bbd65b7
weiyunkj666 <32947593+weiyunkj666@users.noreply.github.com>
2022-07-22 15:56:18 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,7 +255,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "http://127.0.0.1:25500/sub?" }],
+        backendOptions: [{ value: "https://oksubconver-weiyunkj666.cloud.okteto.net/sub?" }],
         remoteConfig: [
           {
             label: "ACL4SSR",
