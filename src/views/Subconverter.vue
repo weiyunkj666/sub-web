8c3a71bb15b04965a995170fa13d5f0f6dc281b8
weiyunkj666 <32947593+weiyunkj666@users.noreply.github.com>
2022-07-24 22:22:04 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,7 +255,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "https://oksubconver-weiyunkj666.cloud.okteto.net/sub?" }],
+        backendOptions: [{ value: "https://weiyunkj-2.herokuapp.com/sub?" }],
         remoteConfig: [
           {
             label: "ACL4SSR",
